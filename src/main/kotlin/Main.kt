e29cb1033550097a7a39713188c949445b9e0637
wancynthianjeri <wanjirucynthianjeri@gmail.com>
2023-06-10 11:29:26 +0300

    commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+
+    val  stmt ="Adam went to the bank"
+    var x =stmt.split("w")
+    println(x)
+
+    val sumn1 ="56"
+    val sumn2 = 56
+    sumn2.toString()
+
+
+
+
+
+}
+
+
+
